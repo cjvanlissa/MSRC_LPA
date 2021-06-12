16fecd390ab75d6d31a9a0dae2a23afa5a047c9b
cjvanlissa <c.j.vanlissa@uu.nl>
2021-06-12 10:10:57 +0200

    update analyses

--- prepare_data.R
+++ prepare_data.R
@@ -5,20 +5,28 @@
 
 library(worcs)
 library(foreign)
+library(haven)
 df <- read.spss("Goodman data 8_19 1.30.21 for Caspar.sav", to.data.frame = T)
+# Retain only cases with 1 attempt or more
+df$exclude <- 0
+df$exclude[which(df$CDE13 > 0)] <- 1
 
 # 3. Military Service Auxiliary Variable - Can you please remove/exclude from analyses the participants for whom it's unclear whether they are currently (vs. previously) serving when looking at military service as an auxiliary variable?
-df <- df[!df$Any_Service == "served but unclear if still serving (cougle)", ]
+df$exclude[df$Any_Service == "served but unclear if still serving (cougle)"] <- 0
+filter_for_emily <- df
+filter_for_emily <- filter_for_emily[, c("Sitecode", "ID", "exclude")]
+haven::write_sav(filter_for_emily, "filter_for_emily.sav")
+df <- df[df$exclude == 1, ]
+df["exclude"] <- NULL
 df$Sitecode <- factor(df$Sitecode)
 df$Gender <- droplevels(df$Gender_MF_Only)
 df$Education <- as.numeric(df$EducationJT)
 df$Method <- droplevels(df$CDE15_Method_Condensed_NEW)
 df$Branch <- factor(gsub("^(Army|Air Force|Navy|Marine Corps|Coast Guard).*$", "\\1", df$Military_Branch))
+
 df[c("ID", "Gender_MF_Only", "EducationJT", "CDE15_CODE", "CDE15_Method_Condensed_NEW", "CDE15_Method_Condensed", "CDE15_Method_More_Condensed", "Military_Branch")] <- NULL
 df[c("CDE7_NEW", "CDE8", "CDE9", "CDE10", "CDE11", "CDE12", "CDE16")] <- lapply(df[c("CDE7_NEW", "CDE8", "CDE9", "CDE10", "CDE11", "CDE12", "CDE16")], as.integer)
 
-# Retain only cases with 1 attempt or more
-df <- df[which(df$CDE13 > 0), ]
 
 desc <- descriptives(df)
 
